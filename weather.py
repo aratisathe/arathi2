dea9e9a2dcf38f223e18f39ef44dc69f043831d0
aratisathe <146555855+aratisathe@users.noreply.github.com>
2025-02-13 13:46:43 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -11,7 +11,7 @@ st.write(
 
 # Function to get weather data
 def get_weather(city):
-    api_key = "3c1d901aba98438bacb44730251302"  # API from weather API
+    api_key = "Enter your API"  # API from weather API
     base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
     
     response = requests.get(base_url)
